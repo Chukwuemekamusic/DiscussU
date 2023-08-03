55f3120d747b08bcb5d7854557631c4f242073c3
emeka <emeka@pop-os.localdomain>
2023-08-03 11:45:17 +0100

    csrf fixed again

--- discussU/settings.py
+++ discussU/settings.py
@@ -162,7 +162,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = BASE_DIR / 'media'
 
-# CSRF_TRUSTED_ORIGINS = ['https://discussu-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://discussu-production.up.railway.app']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
